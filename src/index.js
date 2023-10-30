9a0ac0b34b720bb4d99fd4316b4a392751b2c6ac
Mohd Ebrahim <mohd.ebrahim.jmi@gmail.com>
2023-10-30 15:18:06 +0530

    creating header component and adding external css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
+import "./css/style.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
